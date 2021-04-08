0fbdd5ae7791c1bf1ee364ff6b433c5f4630f94c
ochukochris <chrisochuko101@gmail.com>
2021-04-08 22:45:08 +0100

    test working

--- contracts/test/TrotterNFT.test.js
+++ contracts/test/TrotterNFT.test.js
@@ -1,18 +1,15 @@
-const TrotterNFT = artifacts.require("TrotterNFT");
-const truffleAssert = require("truffle-assertions");
+const TrotterNFT = artifacts.require('TrotterNFT');
+const truffleAssert = require('truffle-assertions');
 
-contract("TrotterNFT", async (accounts) => {
-  it("Returns correct Metadata", async () => {
+contract('TrotterNFT', async (accounts) => {
+  it('Returns correct Metadata', async () => {
     const instance = await TrotterNFT.deployed();
     const contractMetadata = await instance.contractURI.call();
-    
-    assert.equal(
-      contractMetadata,
-      "https://www.trotter.finance/api/NFTs/"
-    );
+
+    assert.equal(contractMetadata, 'https://www.trotter.finance/api/NFTs/');
   });
 
-  it("Sets minter", async () => {
+  it('Sets minter', async () => {
     const instance = await TrotterNFT.deployed();
     const minterRole = await instance.MINTER_ROLE.call();
     await instance.grantRole(minterRole, accounts[1]);
@@ -24,7 +21,7 @@ contract("TrotterNFT", async (accounts) => {
     assert.equal(false, acc2);
   });
 
-  it("Only minter can create token", async () => {
+  it('Only minter can create token', async () => {
     const instance = await TrotterNFT.deployed();
     await instance.addCard(1, { from: accounts[1] });
     await truffleAssert.reverts(instance.addCard(1, { from: accounts[2] }));
@@ -33,7 +30,7 @@ contract("TrotterNFT", async (accounts) => {
     assert.equal(1, created.toNumber());
   });
 
-  it("Cannot mint more than max supply", async () => {
+  it('Cannot mint more than max supply', async () => {
     const instance = await TrotterNFT.deployed();
     await instance.addCard(5, { from: accounts[1] });
     const cardID = await instance.cards.call();
@@ -41,20 +38,20 @@ contract("TrotterNFT", async (accounts) => {
       from: accounts[1],
     });
     await truffleAssert.reverts(
-      instance.mint(accounts[2], cardID.toNumber(), 3, { from: accounts[1] })
+      instance.mint(accounts[2], cardID.toNumber(), 3, { from: accounts[1] }),
     );
     await truffleAssert.reverts(
-      instance.mint(accounts[2], cardID.toNumber(), 1, { from: accounts[2] })
+      instance.mint(accounts[2], cardID.toNumber(), 1, { from: accounts[2] }),
     );
     await instance.mint(accounts[2], cardID.toNumber(), 2, {
       from: accounts[1],
     });
     await truffleAssert.reverts(
-      instance.mint(accounts[2], cardID.toNumber(), 1, { from: accounts[1] })
+      instance.mint(accounts[2], cardID.toNumber(), 1, { from: accounts[1] }),
     );
   });
 
-  it("Burn functions correctly", async () => {
+  it('Burn functions correctly', async () => {
     const instance = await TrotterNFT.deployed();
     let balance = await instance.balanceOf(accounts[2], 2);
     assert.equal(5, balance.toNumber());
@@ -69,12 +66,26 @@ contract("TrotterNFT", async (accounts) => {
     assert.equal(1, circulatingSupply.toNumber());
   });
 
-  it("Create function works", async () => {
+  it('Create function works', async () => {
     const instance = await TrotterNFT.deployed();
     const minterRole = await instance.MINTER_ROLE.call();
     await instance.grantRole(minterRole, accounts[1]);
 
-    await instance.createNftCard('big nft', "QmaZgH4KJT2xZxxPETuUnBvn3aDnEUgjUNwftfA4gk3RSG", 001, accounts[3], 10, 1, { from: accounts[1] });
+    await instance.createNftCard(
+      {
+        name: 'big nft',
+        ipfsHash: 'QmaZgH4KJT2xZxxPETuUnBvn3aDnEUgjUNwftfA4gk3RSG',
+        price: 001,
+        author: 'Chris',
+        about: 'new',
+        properties: '',
+        statement: '',
+      },
+      accounts[3],
+      10,
+      1,
+      { from: accounts[1] },
+    );
     let balance = await instance.balanceOf(accounts[3], 3);
     assert.equal(1, balance.toNumber());
   });
