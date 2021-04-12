5954d43556c25de92b371b35788dad275320b8be
ochukochris <chrisochuko101@gmail.com>
2021-04-12 17:20:10 +0100

    changed key for wallet

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 export default {
     mongoURI: process.env['MONGODB_URL'],
-    wallet: process.env['WALLET'],
+    wallet: process.env['MNEMONIC'],
     adminUsername: process.env['ADMIN_USERNAME'],
     adminPassword: process.env['ADMIN_PASSWORD'],
     nodeEnv: process.env['TROTTER_NODE_ENV']
