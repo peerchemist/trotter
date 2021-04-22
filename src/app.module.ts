cfb49a56821d42f8ec28ee36cb89ab47037722bc
ochukochris <chrisochuko101@gmail.com>
2021-04-22 21:50:25 +0100

    added rate limits

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import config from './config/config';
 import { NftsModule } from './modules/nfts/nfts.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [NftsModule, MongooseModule.forRoot(config.mongoURI)]
+  imports: [NftsModule, MongooseModule.forRoot(config.mongoURI), ThrottlerModule.forRoot({
+    ttl: 60,
+    limit: 10
+  })],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+  ],
 })
 export class AppModule {}
