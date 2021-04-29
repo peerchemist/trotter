8c5ee7dc47de942bc23399cd480ed9873017d089
django <cengiz.kraemer@frontiercargroup.com>
2021-04-29 21:25:43 +0200

    Prevent timing attacks, by safely comparing strings

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,29 +1,34 @@
+/***
+ * Basic authentication to access server api's according to the BASIC authentication scheme https://tools.ietf.org/html/rfc7617
+ * Never use this authentication mechanism on production without SSL/HTTPS
+ * */
+
 import { Request, Response, NextFunction } from 'express';
 import config from '../config/config';
-
-const PRODUCTION_ENV = 'prod';
+import tsscmp from 'tsscmp'
 
 export function auth(req: Request, res: Response, next: NextFunction) {
 
-    if (config.nodeEnv.trim() !== PRODUCTION_ENV) {
+    if (!config.isProductionEnvironment) {
         next();
         return
     }
 
     // check for basic auth header
-    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+    if (!req.headers.authorization || req.headers.authorization.startsWith('Basic ')) {
         return res.status(401).send({ message: 'Access Denied!!' });
     }
 
     // verify auth credentials
-    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const base64Credentials = req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
 
-    // check if api details is valid
-    if (config.adminUsername !== username || config.adminPassword !== password) {
+    // check if api details is valid and prevent timing attacks
+    if (!tsscmp(config.adminUsername, username) || !tsscmp(config.adminPassword !== password)) {
+        res.setHeader('WWW-Authenticate', 'Basic realm="apKey"');
         return res.status(401).json({ message: 'Invalid Authentication Credentials' });
     }
-    
+
     next();
 };
