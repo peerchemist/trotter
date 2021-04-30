6c19de91ef0d4814b32c8cf5399cb9a35637b6fd
django <cengiz.kraemer@frontiercargroup.com>
2021-04-30 19:21:23 +0200

    Wildcard cors for the complete app

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,28 @@ import { auth } from './middleware/auth.middleware';
 import { TrotterLogger } from './utils/logger';
 
 async function bootstrap() {
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: '*',
+  };
   const app = await NestFactory.create(AppModule, {
-    logger: new TrotterLogger()
+    logger: new TrotterLogger(),
     // logger: ['error', 'warn', 'debug', 'log']
   });
-  
+
   const config = new DocumentBuilder()
-  .setTitle('Trotter NFT API.')
-  .setDescription('Trotter does abstraction on creation of NFT tokens on multiple EVM-based blockchain networks.')
-  .setVersion('0.1')
-  .build();
-  
+    .setTitle('Trotter NFT API.')
+    .setDescription('Trotter does abstraction on creation of NFT tokens on multiple EVM-based blockchain networks.')
+    .setVersion('0.1')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
+  app.enableCors(corsOptions);
   app.use(auth);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
