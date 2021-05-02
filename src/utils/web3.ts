bb15d0266595c90537f5ea9c5b8e7b2011aa6c0a
ochukochris <chrisochuko101@gmail.com>
2021-05-02 18:42:00 +0100

    end provider engine after generating hdwallet

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -2,12 +2,17 @@ import config from "src/config/config";
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-const provider = (network: string) => new HDWalletProvider({
-    mnemonic: {
-        phrase: config.wallet
-    },
-    providerOrUrl: config.networks[network]
-});
+const provider = (network: string) => {
+    const prodviderObj = new HDWalletProvider({
+        mnemonic: {
+            phrase: config.wallet
+        },
+        providerOrUrl: config.networks[network]
+    });
+    prodviderObj.engine.stop();
+
+    return prodviderObj;
+}
 
 const web3 = (network: string) => new Web3(provider(network));
 export default web3;
